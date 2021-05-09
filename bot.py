e01a445789d6737eef169f84b4325faef1afd05f
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-05-09 22:24:45 +0200

    feat: made bot compatible with machine

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ class Bot():
     def __init__(self, game):
         self.game = game
         game.render_games()
-        self.look_for_empty()
+        output = self.look_for_empty()
+        print(output)
 
     def look_for_empty(self):
-
         index = 0
         self.over = False
         for rowIndex in range(len(self.game.game_board)):
@@ -24,43 +24,60 @@ class Bot():
                     if columnIndex > 0:
                         #MIDDLE
                         if self.game.game_board[rowIndex][columnIndex - 1] == -2:
-                            self.send_input((rowIndex * self.game.WIDTH) + columnIndex - 1)
+                            number = ((rowIndex * self.game.WIDTH) + columnIndex - 1)
+                            self.send_input(number)
+                            return self.number_to_action(number)
 
                         #TOP
                         if rowIndex > 0:
                             if self.game.game_board[rowIndex - 1][columnIndex - 1] == -2:
-                                self.send_input(((rowIndex - 1)* self.game.WIDTH) + columnIndex - 1)
+                                number = (((rowIndex - 1)* self.game.WIDTH) + columnIndex - 1)
+                                self.send_input(number)
+                                return self.number_to_action(number)
 
                         #BOTTOM
                         if len(self.game.game_board) - 1 > rowIndex:
                             if self.game.game_board[rowIndex + 1][columnIndex - 1] == -2:
-                                self.send_input(((rowIndex + 1) * self.game.WIDTH) + columnIndex - 1)
+                                number = (((rowIndex + 1) * self.game.WIDTH) + columnIndex - 1)
+                                self.send_input(number)
+                                return self.number_to_action(number)
 
                     #RIGHT
                     if len(row) - 1 > columnIndex:
                         #MIDDLE
                         if self.game.game_board[rowIndex][columnIndex + 1] == -2:
-                            self.send_input((rowIndex * self.game.WIDTH) + columnIndex + 1)
+                            number = ((rowIndex * self.game.WIDTH) + columnIndex + 1)
+                            self.send_input(number)
+                            return self.number_to_action(number)
 
                         #TOP
                         if rowIndex > 0:
                             if self.game.game_board[rowIndex - 1][columnIndex + 1] == -2:
-                                self.send_input(((rowIndex - 1)* self.game.WIDTH) + columnIndex + 1)
+                                number = (((rowIndex - 1)* self.game.WIDTH) + columnIndex + 1)
+                                self.send_input(number)
+                                return self.number_to_action(number)
 
                         #BOTTOM
                         if len(self.game.game_board) - 1 > rowIndex:
                             if self.game.game_board[rowIndex + 1][columnIndex + 1] == -2:
-                                self.send_input(((rowIndex + 1) * self.game.WIDTH) + columnIndex + 1)
+                                number = (((rowIndex + 1) * self.game.WIDTH) + columnIndex + 1)
+                                self.send_input(number)
+                                return self.number_to_action(number)
 
                     #MIDDLE
                     #TOP
                     if rowIndex > 0:
                         if self.game.game_board[rowIndex - 1][columnIndex] == -2:
-                            self.send_input(((rowIndex - 1)* self.game.WIDTH) + columnIndex)
+                            number = (((rowIndex - 1)* self.game.WIDTH) + columnIndex)
+                            self.send_input(number)
+                            return self.number_to_action(number)
 
+                    #BOTTOM
                     if len(self.game.game_board) - 1 > rowIndex:
                         if self.game.game_board[rowIndex + 1][columnIndex] == -2:
-                            self.send_input(((rowIndex - 1)* self.game.WIDTH) + columnIndex)
+                            number = (((rowIndex + 1)* self.game.WIDTH) + columnIndex)
+                            self.send_input(number)
+                            return self.number_to_action(number)
 
 
 
